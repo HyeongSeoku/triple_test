37598ee0a0661161f79fcff655d43a81f2ca4090
HyeongSeoku <gudtjr3437@naver.com>
2022-06-24 12:58:12 +0900

    Chore : Add svg files

---
+++ src/assets/svgs/index.tsx
@@ -0,0 +1,4 @@
+export { ReactComponent as Logo } from './logo.svg'
+export { ReactComponent as Apple } from './apple.svg'
+export { ReactComponent as PlayStore } from './playStore.svg'
+export { ReactComponent as TripleLogo } from './tripleLogo.svg'
